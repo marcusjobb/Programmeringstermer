e8066a8004233061b25079d5196aad7934d80255
Marcus Medina <medina.jobb@outlook.com>
2021-08-06 12:59:59 +0200

    Added inparameter

--- Programmeringstermer/Exempel/Klasser/Inparameter.cs
+++ Programmeringstermer/Exempel/Klasser/Inparameter.cs
@@ -6,8 +6,18 @@ using System.Threading.Tasks;
 
 namespace Programmeringstermer.Exempel.Klasser
 {
-    public class Inparameter_demo
+public class Inparameter_demo
+{
+    public Inparameter_demo()
     {
-        
+        CallMe("maybe");
     }
+
+    // "option" är en inparameter
+    private void CallMe(string option)
+    {
+        Console.WriteLine("Hey, I just met you. And this is crazy.");
+        Console.WriteLine("But here's my number. So call me, " + option + "?");
+    }
+}
 }
