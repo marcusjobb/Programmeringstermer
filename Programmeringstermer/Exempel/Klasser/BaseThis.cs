0e78a2d0e76c58ee7366f827b217f692e11bcf37
Marcus Medina <medina.jobb@outlook.com>
2021-08-09 12:22:21 +0200

    Base och polymorphism

---
+++ Programmeringstermer/Exempel/Klasser/BaseThis.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmeringstermer.Exempel.Klasser.BaseThis
+
+{
+    class MotherClass
+    {
+        public virtual string Name { get; set; } = "MotherClass";
+    }
+    class ChildClass : MotherClass
+    {
+        public override string Name{ get => this.Name; set => this.Name = value; }
+        public string Mother{ get => base.Name; set => base.Name = value; }
+        public ChildClass()
+        {
+            this.Name = "ChildClass";
+        }
+    }
+    public class Test
+    {
+        public void Run()
+        {
+            var child = new ChildClass();
+            Console.WriteLine("Mother name  : " + child.Mother);
+            Console.WriteLine("Child name   : " + child.Name);
+        }
+    }
+}
